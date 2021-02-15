f7a753141695c95278c05e051af81fcd7fe2926e
kostyamorales <kostyamorales@gmail.com>
2021-02-15 15:14:29 +0300

    Linked flats with owners

--- property/models.py
+++ property/models.py
@@ -5,9 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
-    owners_phonenumber = models.CharField("Номер владельца", max_length=20)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', null=True, blank=True, max_length=20)
     new_building = models.NullBooleanField("Новостройка", db_index=True)
     created_at = models.DateTimeField("Когда создано объявление", default=timezone.now, db_index=True)
 
